efbf57c1235a3b40ca13825c2b459b37b6805f28
syxme <systemxmedias@gmail.com>
2024-10-23 02:29:37 +0400

    -

--- src/graphics/Bitmap.ts
+++ src/graphics/Bitmap.ts
@@ -1,3 +1,4 @@
+import {Rect} from "@casperui/core/graphics/Rect";
 
 export class Bitmap {
     private image: HTMLImageElement | OffscreenCanvas;
@@ -64,7 +65,12 @@ export class Bitmap {
             ctx.putImageData(data,0,0)
         }
     }
-
+    setPixelsDirty(data:ImageData,rect:Rect){
+        if (this.image instanceof OffscreenCanvas){
+            const ctx = this.image.getContext('2d');
+            ctx.putImageData(data,0,0,rect.mLeft,rect.mTop,rect.getWidth(),rect.getHeight());
+        }
+    }
     getWidth(): number {
         return this.image.width
     }
